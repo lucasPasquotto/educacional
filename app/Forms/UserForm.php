9ef62b424d056bbcf5903570b10d704e91626234
Lucas Pasquotto <lucaspasquotto@outlook.com>
2022-11-03 11:23:27 -0300

    Auth, Rotas ADM, Form User

---
+++ app/Forms/UserForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+
+class UserForm extends Form
+{
+    public function buildForm()
+    {
+        $this
+            ->add('name', 'text')
+            ->add('email', 'email');
+    }
+}
